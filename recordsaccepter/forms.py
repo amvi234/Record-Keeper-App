a30fce986aab7edb655985369b1fde1ecd92e5f4
Amit Vikram <amitv@aereo.io>
2024-05-29 11:13:04 +0530

    feat:  auto add today's date

--- recordsaccepter/forms.py
+++ recordsaccepter/forms.py
@@ -1,7 +1,8 @@
+import datetime
 from django import forms
 
 class Records(forms.Form):
     id = forms.CharField(label="issuer id", max_length=100)
     name = forms.CharField(label="issuer name", max_length=100)
     description = forms.CharField(label="record description", max_length=100)
-    date = forms.DateField(label="issue date")
+    date = forms.DateField(initial=datetime.date.today, label="issue date")
